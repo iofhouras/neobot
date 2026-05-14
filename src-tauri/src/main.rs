fd7226562ae84db2a51a424a45770118d7e70102
iofhouras <160026327+iofhouras@users.noreply.github.com>
2026-05-14 12:09:51 -0500

    Register Windows download commands in main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,9 +9,11 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
-            commands::provision::download_and_import_kali_ova,
-            commands::provision::get_kali_download_path,
-            // Add other commands here (vm_control, etc.)
+            commands::provision::setup_kali_vm,
+            commands::provision::get_kali_path,
+            commands::windows::download_virtualbox_for_windows,
+            commands::windows::download_neobot_installer_for_windows,
+            commands::windows::get_windows_download_path,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
